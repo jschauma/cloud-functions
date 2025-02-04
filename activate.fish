572a16a560e2f40bb9a3079c12c548870fcc447f
Anthony Webster <ajwebster1@gmail.com>
2025-02-03 19:11:38 -0500

    Handle when script is invoked through a symlink

--- activate.fish
+++ activate.fish
@@ -1,4 +1,9 @@
-set dir (realpath (status dirname))
+# We have to do some prancing around in case we're navigating through a symlink
+set dir (status dirname)
+set f (status filename)
+set fullname "$dir/$f"
+set fullname (realpath -- $fullname)
+set dir (dirname -- $fullname)
 if test -z $dir
     set dir .
 end
